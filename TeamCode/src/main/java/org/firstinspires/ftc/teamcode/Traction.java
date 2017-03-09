78636151bdc31f68a3a8651958ddf2cafdb1c24f
QUBE <qube@cnmv.ro>
2017-03-09 13:55:21 +0200

    Add new Traction enum

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Traction.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+/// Describes which of the robot's drive motors are used.
+public enum Traction {
+    /// Use only the back drive motors.
+    Back,
+    /// Use only the front drive motors;
+    Front,
+    /// Use all motors (default).
+    Both
+}
