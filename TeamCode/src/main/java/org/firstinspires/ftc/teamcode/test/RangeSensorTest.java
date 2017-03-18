f6ffceeb1b43e57497fd7bd7e9b21a4a9b150ec4
QUBE <qube@cnmv.ro>
2017-03-18 12:31:04 +0200

    Update range sensor test opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RangeSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RangeSensorTest.java
@@ -1,65 +1,38 @@
-/*
-Copyright (c) 2016 Robert Atkinson
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Robert Atkinson nor the names of his contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 package org.firstinspires.ftc.teamcode.test;
 
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.I2cAddrConfig;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.LinearRobotOpMode;
+import org.firstinspires.ftc.teamcode.sensor.RangeSensor;
 
 
-@TeleOp(name = "RangeSensorTest", group = "Tests")
-//@Disabled   // comment out or remove this line to enable this opmode
-public class RangeSensorTest extends LinearOpMode {
+@Autonomous (name = "Range Sensor Test", group = "Tests")
+public class RangeSensorTest extends LinearRobotOpMode {
 
-    ModernRoboticsI2cRangeSensor usdSensorRightLeft;
+    private void addUSDSensorData(RangeSensor sensor, String name) {
+        telemetry.addData(name + " raw ultrasonic", sensor.rawUltrasonic());
+        telemetry.addData(name + " raw optical", sensor.rawOptical());
+        telemetry.addData(name + " cm optical", "%.2f cm", sensor.cmOptical());
+        telemetry.addData(name + " cm", "%.2f cm", sensor.getDistance(DistanceUnit.CM));
+    }
 
-    @Override public void runOpMode() {
-
+    @Override
+    public void play() {
         // get a reference to our compass
-        usdSensorRightLeft = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "usdSensorRightLeft.");
+        robot.initRangeSensors();
 
         // wait for the start button to be pressed
         waitForStart();
 
         while (opModeIsActive()) {
-            telemetry.addData("raw ultrasonic", usdSensorRightLeft.rawUltrasonic());
-            telemetry.addData("raw optical", usdSensorRightLeft.rawOptical());
-            telemetry.addData("cm optical", "%.2f cm", usdSensorRightLeft.cmOptical());
-            telemetry.addData("cm", "%.2f cm", usdSensorRightLeft.getDistance(DistanceUnit.CM));
+            addUSDSensorData(robot.usdSensorFrontLeft, "Front left sensor");
+            addUSDSensorData(robot.usdSensorFrontRight, "Front right sensor");
+            addUSDSensorData(robot.usdSensorRightLeft, "Front left sensor");
             telemetry.update();
         }
     }
