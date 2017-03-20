0ac8e43f7c4f020f36ac5c2f1097f8285483c34f
QUBE <qube@cnmv.ro>
2017-03-20 16:35:11 +0200

    Extract PID rotation function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearRobotOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearRobotOpMode.java
@@ -32,6 +32,36 @@ public abstract class LinearRobotOpMode extends LinearOpMode {
         }
     }
 
+    private final double P = 30,
+            I = 10,
+            D = 60,
+            scale = 0.05;
+
+    public void rotateTo(double direction) {
+        double angle = robot.gyro.getIntegratedZValue();
+        double error  = direction - angle;
+        double lastError = error;
+        boolean arrived = false;
+        double timer = 50;
+        runtime.reset();
+        while (opModeIsActive() && ((runtime.time() - timer < 2) || arrived == false)){
+            //PID
+            double  motorCorrection = (((P * error) + (I * (error + lastError)) + D * (error - lastError)) * scale) / 100;
+
+            robot.tractiuneIntegrala(- motorCorrection, motorCorrection);
+
+            lastError = error;
+            angle = robot.gyro.getIntegratedZValue();
+            error = direction - angle;
+            telemetry.addData("Angle", "%f", angle);
+            telemetry.addData("Error", "%f", error);
+            update();
+            if(error <= 10 && error >= -10 && arrived == false){
+                arrived = true;
+                timer = runtime.time();
+            }
+        }
+    }
 
     /// This function is to be overridden in real op-modes.
     protected abstract void play();
