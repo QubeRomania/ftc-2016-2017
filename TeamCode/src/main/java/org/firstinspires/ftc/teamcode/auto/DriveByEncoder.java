ba5dafd91ee6b8c1bfbdf3395388a519ab45b6b4
QUBE <qube@cnmv.ro>
2017-03-20 16:37:32 +0200

    Try encoders again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/DriveByEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/DriveByEncoder.java
@@ -3,15 +3,18 @@ package org.firstinspires.ftc.teamcode.auto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.teamcode.LinearRobotOpMode;
 import org.firstinspires.ftc.teamcode.RobotOpMode;
 
 @Autonomous(name="Encoder Drive Test", group="Tests")
-public final class DriveByEncoder extends RobotOpMode {
+public final class DriveByEncoder extends LinearRobotOpMode {
 
-    private static final double ROTATIONS_PER_MOTOR_REV = 0;
+    // AndyMark motors.
+    private static final double ROTATIONS_PER_MOTOR_REV = 1220;
 
-    private static final double DRIVE_GEAR_REDUCTION = 1;
+    private static final double DRIVE_GEAR_REDUCTION = 2;
 
     private static final double WHEEL_DIAMETER_CM = 10;
 
@@ -19,43 +22,48 @@ public final class DriveByEncoder extends RobotOpMode {
 
     private static final double ROTATIONS_PER_CM = (ROTATIONS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION)
             / WHEEL_CIRCUMFERENCE_CM;
-    private double distance = 50;
 
     @Override
-    public void start() {
+    public void play() {
         robot.initDriveMotors();
         robot.initFireMotors();
+
+        // Reset back motors.
+
         robot.leftBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.rightBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.leftBackMotor.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        robot.rightBackMotor.setMode(DcMotor.RunMode.RESET_ENCODERS);
+
+        robot.leftBackMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBackMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        idle();
+        waitForStart();
+
+        driveByEncoder(10);
     }
 
-    private void driveByEncoder(double distance) {
+    private void driveByEncoder(double distanceInCm) {
+        int targetPosition = (int)(distanceInCm * ROTATIONS_PER_CM);
 
-        robot.leftBackMotor.setTargetPosition((int)distance);
-        robot.rightBackMotor.setTargetPosition((int)distance);
+        robot.leftBackMotor.setTargetPosition(targetPosition);
+        robot.rightBackMotor.setTargetPosition(targetPosition);
 
         robot.leftBackMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.rightBackMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        while(robot.leftBackMotor.isBusy() && robot.rightBackMotor.isBusy()){
-            robot.tractiuneIntegrala(0.7, 0.7);
-            telemetry.addData("Status", "ne deplasam");
-            telemetry.addData("Pozitie: ", "%d", robot.leftBackMotor.getCurrentPosition());
-            telemetry.update();
+        robot.tractiuneIntegrala(0.2, 0.2);
+
+        while(robot.leftBackMotor.isBusy() && robot.rightBackMotor.isBusy()) {
+            setStatus("Moving");
+            telemetry.addData("Position: ", "%d", robot.leftBackMotor.getCurrentPosition());
+            update();
         }
 
         robot.tractiuneIntegrala(0, 0);
 
-        telemetry.addData("Status", "Am ajuns");
-        telemetry.update();
-    }
+        setStatus("Position reached.");
+        update();
 
-    @Override
-    public void loop() {
-        driveByEncoder(distance);
-
-        requestOpModeStop();
+        waitForMs(1000);
     }
 }
