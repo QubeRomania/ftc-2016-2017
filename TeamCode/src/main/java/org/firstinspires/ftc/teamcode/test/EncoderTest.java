bfb9e3c2bfdd21f7e700a5771b2cd97469418978
QUBE <qube@cnmv.ro>
2017-03-20 16:36:25 +0200

    Add test for firing with encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/EncoderTest.java
@@ -1,73 +1,58 @@
-/* Copyright (c) 2015 Qualcomm Technologies Inc
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Qualcomm Technologies Inc nor the names of its contributors
-may be used to endorse or promote products derived from this software without
-specific prior written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
-
 package org.firstinspires.ftc.teamcode.test;
 
-import android.app.Activity;
-import android.graphics.Color;
-import android.view.View;
-
-import com.qualcomm.ftcrobotcontroller.R;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.LinearRobotOpMode;
 
-@TeleOp(name = "EncoderTest", group = "Tests")
-//@Disabled
-
+@TeleOp(name = "Encoder Test", group = "Tests")
 public class EncoderTest extends LinearRobotOpMode {
 
+    private static final int REVS_PER_ROTATION = 1220;
 
+    private static final int LAUNCH_SPEED = (int)(REVS_PER_ROTATION * 0.85);
 
     @Override
     public void play() {
-        robot.initFireMotors();
+        robot.initAllMotors();
+        robot.initServos();
+
+        robot.fireRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        robot.fireLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.fireRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        idle();
+
+        robot.fireLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.fireRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
-        while (opModeIsActive()){
-            robot.fireLeftMotor.setMaxSpeed(0);
-            robot.fireLeftMotor.setMaxSpeed(0);
+        robot.fireLeftMotor.setMaxSpeed(LAUNCH_SPEED);
+        robot.fireRightMotor.setMaxSpeed(LAUNCH_SPEED);
 
-            if(gamepad1.a){
-                robot.fireLeftMotor.setMaxSpeed(1);
-                robot.fireRightMotor.setMaxSpeed(1);
+        robot.fireLeftMotor.setPower(0.78);
+        robot.fireRightMotor.setPower(0.78);
+
+        while (opModeIsActive()) {
+
+            if (gamepad1.right_trigger > 0.5) {
+                robot.servoFire.setFiring(true);
+            }
+            else {
+                robot.servoFire.setFiring(false);
             }
 
+            if (gamepad1.left_trigger > 0.5) {
+                robot.grabBallsv2(gamepad1.left_trigger);
+            } else {
+                robot.grabBallsv2(0);
+            }
 
-            robot.fireLeftMotor.setPower(0.78);
-            robot.fireRightMotor.setPower(0.78);
+            telemetry.addData("Speed", "%d revs", robot.fireLeftMotor.getMaxSpeed());
+            update();
         }
 
     }
